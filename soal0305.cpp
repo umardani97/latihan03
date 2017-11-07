6098b00e51193029bf488aa8a088dbcd31885e58
umardani97 <32365705+umardani97@users.noreply.github.com>
2017-11-07 23:37:27 +0800

    menghitung Underweight

---
+++ soal0305.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int tb, bb, hasil1, hasil2;
+	cout<<"Menghitung Badan Ideal"<<endl;
+	cout<<"Masukkan Tinggi Badan = ";cin>>tb;
+	cout<<"Masukkan Berat Badan  = ";cin>>bb;
+	
+	if(bb<tb/2.5){
+		cout<<"Anda Termasuk UNDERWEIGHT";
+	}
+}
