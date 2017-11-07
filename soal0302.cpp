829787fce79e094594e8307c2e0912dc4c0b5296
umardani97 <32365705+umardani97@users.noreply.github.com>
2017-11-07 23:24:59 +0800

    mencari luas persegi panjang

---
+++ soal0302.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x, y, z, p;
+	char r;
+	
+	cout<<"Menghitung Luas Persegi Panjang dan Segitiga"<<endl;
+	cout<<"Klik 1 Untuk Menghitung Persegi panjang:  "<<endl;
+	cout<<"Klik 2 Untuk Menghitung Segitiga       :  "<<endl;
+	cout<<"pilih>>>>   ";cin>>p;
+	
+	if (p==1){
+		cout<<"Masukan panjang persegi panjang:";cin>>x;
+		cout<<"Masukan lebar persegi panjang  :";cin>>y;
+		z=y*x;
+		cout<<"Luas Persegi Panjang           :"<<z<<"cm2"<<endl;
+	}
+}
